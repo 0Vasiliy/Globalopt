2c9f1fd08b23754ae98891eb034c615db8c5e6c2
Козин-Поздняков Василий <vasiliy_pit@mail.ru>
2022-12-10 21:53:07 +0300

    scroll_js

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,26 +1,26 @@
- window.addEventListener('DOMContentLoaded',function(){
-     const links = document.querySelectorAll('.menu-item');
-     for(let i = 0; i < links.length; i++){
-        console.log(links[i]);
-     };
+document.addEventListener('DOMContentLoaded',function(){
+    const links = document.querySelectorAll('.menu-item');
+    for(let i = 0; i < links.length; i++){
+       console.log(links[i]);
+    }
+  
+     
    
-      
-    
-    
+   
 //     const menu = document.querySelector('.menu'),
 //           phoneMenu = document.querySelector('.menu__phone'),
 //           closeMenu = document.querySelector('.menu__close');
-    
-    
+   
+   
 //         //   mobile menu
 //     phoneMenu.addEventListener('click', () => {
 //         phoneMenu.classList.add('active');
 //         menu.classList.add('active');
-    
+   
 //     });
 //     closeMenu.addEventListener('click', () => {
 //         phoneMenu.classList.remove('active');
 //         menu.classList.remove('active');
 //     });
-     
+    
 })  
